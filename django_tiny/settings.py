90b3152af37651701f2d886113587dcef8927b02
JGONGSQ <james90jiajia@Hotmail.com>
2015-09-20 15:29:10 +0800

    start up on index.html

--- django_tiny/settings.py
+++ django_tiny/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'bootstrap3',
+    'django_comments',
+    'masquerade',
 )
 
 MIDDLEWARE_CLASSES = (
